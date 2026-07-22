e780500d523267e1df3aa918415e5a08d3173239
cc18168 <13926581994@163.com>
2026-07-22 19:05:10 +0800

    Export cloud ASR provider clients

--- projects/cineflow-cloud/cineflow/providers/__init__.py
+++ projects/cineflow-cloud/cineflow/providers/__init__.py
@@ -1,5 +1,15 @@
+from .aliyun_asr import AliyunFunASRClient, AliyunFunASRConfig
 from .contracts import PipelineProviders
 from .demo import DemoProviders
 from .http_bundle import ProductionProviders
+from .volcengine_asr import VolcengineASRConfig, VolcengineFlashASRClient
 
-__all__ = ["DemoProviders", "PipelineProviders", "ProductionProviders"]
+__all__ = [
+    "AliyunFunASRClient",
+    "AliyunFunASRConfig",
+    "DemoProviders",
+    "PipelineProviders",
+    "ProductionProviders",
+    "VolcengineASRConfig",
+    "VolcengineFlashASRClient",
+]
