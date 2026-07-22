1b1fd82c61e042daf0e37cc4137b85b4aa599923
cc18168 <13926581994@163.com>
2026-07-22 19:41:17 +0800

    Export Alibaba media provider components

--- projects/cineflow-cloud/cineflow/providers/__init__.py
+++ projects/cineflow-cloud/cineflow/providers/__init__.py
@@ -1,4 +1,7 @@
 from .aliyun_asr import AliyunFunASRClient, AliyunFunASRConfig
+from .aliyun_ice import AliyunICEClient, AliyunICEConfig
+from .aliyun_media import AliyunMediaConfig, AliyunMediaService
+from .aliyun_oss import AliyunOSSConfig, AliyunOSSStore
 from .contracts import PipelineProviders
 from .demo import DemoProviders
 from .http_bundle import ProductionProviders
@@ -7,6 +10,12 @@ from .volcengine_asr import VolcengineASRConfig, VolcengineFlashASRClient
 __all__ = [
     "AliyunFunASRClient",
     "AliyunFunASRConfig",
+    "AliyunICEClient",
+    "AliyunICEConfig",
+    "AliyunMediaConfig",
+    "AliyunMediaService",
+    "AliyunOSSConfig",
+    "AliyunOSSStore",
     "DemoProviders",
     "PipelineProviders",
     "ProductionProviders",
